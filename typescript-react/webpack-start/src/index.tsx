955b724c391e4680883c39890098ed82711b60df
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-02-29 13:24:13 +0900

    feat: todo by usestate

--- typescript-react/webpack-start/src/index.tsx
+++ typescript-react/webpack-start/src/index.tsx
@@ -1,14 +1,44 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
 export default function App(): JSX.Element {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string) => {
+    setTodos([...todos, { text: text, complete: false }]);
+  };
+
+  const todoElems = todos.map((todo, index) => {
+    return <li key={index.toString()}>{todo.text}</li>;
+  });
+
   return (
     <Fragment>
       <h1>Todo List</h1>
-      <form>
-        <input type="text" required />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value.toString())}
+          required
+        />
         <button type="submit">Add Todo</button>
       </form>
+      <ul>{todoElems}</ul>
     </Fragment>
   );
 }
