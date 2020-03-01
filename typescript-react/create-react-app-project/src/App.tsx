e2de96fbe651fb3f9076d82220302d7b8f7d52ed
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-03-01 14:03:58 +0900

    feat: context

--- typescript-react/create-react-app-project/src/App.tsx
+++ typescript-react/create-react-app-project/src/App.tsx
@@ -1,13 +1,57 @@
 import React from 'react'
 import { Store } from './Store'
 
+interface IEpisode {
+  airdate: string
+  id: number
+  image: {
+    medium: string
+    original: string
+  }
+  name: string
+  number: number
+  season: number
+  summary: string
+  url: string
+}
+
 export default function App(): JSX.Element {
-  const store = React.useContext(Store)
+  const { state, dispatch } = React.useContext(Store)
+
+  // Reactにおけるいろんな描画処理タイミングがまとまったもの
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction()
+  })
+
+  const fetchDataAction = async () => {
+    const data = await fetch(
+      'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+    )
+    const json = await data.json()
+
+    // Actionsの実行
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: json._embedded.episodes
+    })
+  }
+
   return (
     <React.Fragment>
-      {console.log(store)}
-      <h1>test</h1>
-      <p>test</p>
+      <h1>TVMaze</h1>
+      <section>
+        {state.episodes.map((episode: IEpisode) => {
+          return (
+            <section key={episode.id}>
+              <img src={episode.image.medium} alt={`name: ${episode.name}`} />
+              <div>{episode.name}</div>
+              <section>
+                Season: {episode.season}, Number: {episode.number}
+              </section>
+            </section>
+          )
+        })}
+      </section>
     </React.Fragment>
   )
 }
--- typescript-react/create-react-app-project/src/Store.tsx
+++ typescript-react/create-react-app-project/src/Store.tsx
@@ -5,17 +5,34 @@ interface IState {
   favoraites: []
 }
 
+interface IAction {
+  type: string
+  payload: any
+}
+
 const initialState: IState = {
   episodes: [],
   favoraites: []
 }
 
-export const Store = React.createContext(initialState)
+export const Store = React.createContext<IState | any>(initialState)
 
-// const reducer = () => {
-//   // pass
-// }
+// ReducersとStoreの実装
+const reducer = (state: IState, action: IAction): IState => {
+  switch (action.type) {
+    case 'FETCH_DATA':
+      return { ...state, episodes: action.payload }
+    default:
+      return state
+  }
+}
 
 export const StoreProvider = (props: any): JSX.Element => {
-  return <Store.Provider value={initialState}>{props.children}</Store.Provider>
+  // ReducersとStoreの設定（Actionsの実装）
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+  return (
+    <Store.Provider value={{ state, dispatch }}>
+      {props.children}
+    </Store.Provider>
+  )
 }
