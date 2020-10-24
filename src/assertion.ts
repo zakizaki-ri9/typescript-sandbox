ab94e4aad1941d7a995a1ee17a9e20aa0a7fc15e
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-10-25 08:45:42 +0900

    root src delete

--- src/assertion.ts
+++
@@ -1,9 +0,0 @@
-export function sample() {
-  let someValue: any = "this is a string"
-
-  // <type>でアサーション
-  let a1: number = (<string>someValue).length
-  
-  // asでアサーション
-  let a2: number = (someValue as string).length
-}
