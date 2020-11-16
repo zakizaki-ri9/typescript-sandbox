3101754277188ebdf5978a92f187e95ab7af216d
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-11-16 11:24:09 +0900

    feat: linkedlist aplindrome

---
+++ leetcode/src/LinkedList/AplindromeLinkedList.ts
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/772/
+ * の回答
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+// main
+function isPalindrome(head: ListNode | null): boolean {
+  const vals = getVals(head);
+  if (!vals) return false;
+
+  for (let i = 0; i < vals.length / 2; i++) {
+    if (vals[i] !== vals[vals.length - i - 1]) return false;
+  }
+
+  return true;
+}
+
+function getVals(head: ListNode | null): number[] {
+  if (!head) return [];
+
+  const vals: number[] = [head.val];
+  let nextNode = head.next;
+  while (nextNode) {
+    vals.push(nextNode.val);
+    nextNode = nextNode.next;
+  }
+  return vals;
+}
---
+++ leetcode/src/LinkedList/README.md
@@ -0,0 +1,3 @@
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list
+
+の回答集
