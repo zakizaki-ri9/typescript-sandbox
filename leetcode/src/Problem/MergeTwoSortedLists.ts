8d8fb88025ab1b8a84559cbe8cbc09aea711eda0
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-12-28 21:53:02 +0900

    refactor: tsconfig

--- leetcode/src/Problem/MergeTwoSortedLists.ts
+++ leetcode/src/Problem/MergeTwoSortedLists.ts
@@ -1,3 +1,12 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
 export function mergeTwoLists(
   l1: ListNode | null,
   l2: ListNode | null
