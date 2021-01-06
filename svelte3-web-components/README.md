5004362cf5583873110405b8aa3e1a504c51e1ad
zakizaki-ri9 <kic.yuuki@gmail.com>
2021-01-06 09:37:23 +0900

    add ts

---
+++ svelte3-web-components/README.md
@@ -0,0 +1,21 @@
+[いまからはじめる Svelte3](https://techbookfest.org/product/4925576424980480)の素振り
+
+# 動かし方
+
+```bash
+# ローカル環境
+npm i
+npm run dev
+```
+
+# メモ
+
+## svelte
+
+REPL環境があるそう、お試しにいいかも。
+
+- https://svelte.dev/repl/hello-world?version=3
+
+## degit
+
+- [npm](https://www.npmjs.com/package/degit)
--- svelte3-web-components/rollup.config.js
+++ svelte3-web-components/rollup.config.js
@@ -4,6 +4,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import autoPreprocess from 'svelte-preprocess';
+import typescript from '@rollup/plugin-typescript';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -29,7 +31,7 @@ function serve() {
 }
 
 export default {
-	input: 'src/main.js',
+	input: 'src/main.ts',
 	output: {
 		sourcemap: true,
 		format: 'iife',
@@ -41,8 +43,10 @@ export default {
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
-			}
-		}),
+      },
+      preprocess: autoPreprocess()
+    }),
+    typescript({sourceMap: !production}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
