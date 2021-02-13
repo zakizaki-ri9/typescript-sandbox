cacccba618b126c1e5085829ad355dcf708f04b1
zakizaki-ri9 <kic.yuuki@gmail.com>
2021-02-13 10:18:27 +0900

    add about

---
+++ my-gatsby-site/src/pages/about.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AbountPage: React.FC = () => {
+  return <p>My Profile</p>;
+};
+
+export default AbountPage;
--- my-gatsby-site/src/pages/index.tsx
+++ my-gatsby-site/src/pages/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const IndexPage: React.FC = () => {
-  return <p>Hello Gatsby</p>;
+  return (
+    <div>
+      <p>Hello Gatsby</p>
+      <Link to="/about">Link to About</Link>
+    </div>
+  );
 };
 
 export default IndexPage;
