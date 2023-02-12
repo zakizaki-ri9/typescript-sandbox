a9789d618a4a1124bae8a21f2112b34b261c2772
zakizaki-ri9 <kic.yuuki@gmail.com>
2023-02-12 14:55:25 +0900

    remove console log

--- astro-practice/astro.config.mjs
+++ astro-practice/astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config"
 import UnoCSS from "@unocss/astro"
-import {config} from "../nuxt3-unocss-storybook/unocss.config"
+import { config } from "../nuxt3-unocss-storybook/unocss.config"
 
 // https://astro.build/config
 import vue from "@astrojs/vue"
-
-console.log({
-  config,
-})
 
 // https://astro.build/config
 export default defineConfig({
