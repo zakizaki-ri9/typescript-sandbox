95b6c9608b7ad0a33ed24ad01ff4c7a77c84e84b
zakizaki-ri9 <kic.yuuki@gmail.com>
2023-02-24 22:11:59 +0900

    static

--- frontend-security-study/src/index.ts
+++ frontend-security-study/src/index.ts
@@ -2,6 +2,8 @@ import express, {Application} from "express";
 const app: Application = express();
 const port = 3000;
 
+app.use(express.static("public"))
+
 app.get("/", (_req, res, _next) => {
   res.end("Top page")
 })
