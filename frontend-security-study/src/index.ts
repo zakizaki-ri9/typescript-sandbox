4fd9af70224a380a566b1d33a38c3f27e0428456
zakizaki-ri9 <kic.yuuki@gmail.com>
2023-03-20 08:52:39 +0900

    replace tsx

--- frontend-security-study/src/index.ts
+++ frontend-security-study/src/index.ts
@@ -1,7 +1,9 @@
-import express, {Application} from "express";
+import express, { Application } from "express";
+import {router} from "@/routes/api"
 const app: Application = express();
 const port = 3000;
 
+app.use("/api", router);
 app.use(express.static("public"))
 
 app.get("/", (_req, res, _next) => {
