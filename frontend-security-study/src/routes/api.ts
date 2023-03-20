6f77f8d078c0d773868c598aff483eb1b40c773f
zakizaki-ri9 <kic.yuuki@gmail.com>
2023-03-20 10:13:24 +0900

    fat: 3.3.1

--- frontend-security-study/src/routes/api.ts
+++ frontend-security-study/src/routes/api.ts
@@ -1,10 +1,32 @@
 import express from "express"
+import { IncomingHttpHeaders } from "http";
 
 const router = express.Router();
 
-router.get("/", (_, res) => {
-  res.send({message: "Hello"})
+router.get("/get", (req, res) => {
+  res.setHeader("X-Timestamp", Date.now())
+
+  if (req.query.message === "") {
+    res.status(400);
+  }
+  res.send({
+    message: generateMessage(req.query.message, req.headers["x-lang"]),
+  });
 })
+
+const generateMessage = (
+  queryMessage: qs.ParsedQs[string],
+  lang: IncomingHttpHeaders[string]
+) => {
+  if (queryMessage !== "") {
+    return queryMessage;
+  }
+
+  if (lang === "en") {
+    return "message is empty.";
+  }
+  return "message の値が空です。";
+};
 
 export {
   router
