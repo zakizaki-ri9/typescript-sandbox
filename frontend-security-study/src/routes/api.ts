2ef3b54b4f8a675e8b9ea1d409a8efa14cf3b1f0
zakizaki-ri9 <kic.yuuki@gmail.com>
2023-05-07 09:57:50 +0900

    プリフライトリクエスト対応

--- frontend-security-study/src/routes/api.ts
+++ frontend-security-study/src/routes/api.ts
@@ -3,7 +3,17 @@ import { IncomingHttpHeaders } from "http";
 
 const router = express.Router();
 
-router.get("/get", (req, res) => {
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  // プリフライトリクエスト時送信可能なヘッダーを設定
+  // ex. await fetch("http://site.example:3000/api", { headers: { "X-Token": "test" }});
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Headers", "X-Token");
+  }
+  next();
+})
+
+router.get("/", (req, res) => {
   res.setHeader("X-Timestamp", Date.now())
 
   if (req.query.message === "") {
